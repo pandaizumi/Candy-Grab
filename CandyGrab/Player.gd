ca967fac712789ad4ed1652fb8484f2bfb52e06f
XL'anna Brooks <pandajunkay@yahoo.com>
2020-09-21 15:50:46 -0400

    Player/Asset Setup

---
+++ CandyGrab/Player.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+export var speed = 150
+var screen_size
+
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+
+func start(pos):
+	position = pos
+
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		$Anim.play("right")
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		$Anim.play("left")
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		$Anim.play("down")
+	elif Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		$Anim.play("up")
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	else:
+		$Anim.stop()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0 + 6, screen_size.x - 8)
+	position.y = clamp(position.y, 0 + 7, screen_size.y - 16)
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Candy-Grab
 A Pickin' Sticks (by Moosader) by clone , but with candy made with Godot to get familiar with the engine.
+
+
+player sprite - pipoya (https://pipoya.itch.io/)
+grass and candy sprites - Rachel Singh (https://moosader.com)
+Kenny Future Narrow Font - Kenney (https://kenney.nl/assets)
+Where Time Stands Still.ogg - Juhani Junkala (https://juhanijunkala.com/)
